62d0ae1fe9402a36ab8d4d0adb52c5ba5a297f31
Surendar Singh <surendarsingh2002@gmail.com>
2024-07-14 21:59:46 +0530

    feat: add lift-simulation ui

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ document.addEventListener("DOMContentLoaded", async () => {
       alert("Number of lifts should be between 1 and 10");
       return;
     }
+    if (totalLifts > totalFloors) {
+      alert("Why do you need more lifts than floors?");
+      return;
+    }
 
     loadLiftSimulation(totalFloors, totalLifts);
   });
@@ -59,4 +63,67 @@ async function loadLiftSimulation(totalFloors, totalLifts) {
 
   const liftSimulationPara = document.getElementById("lift-simulation-para");
   liftSimulationPara.textContent = `Lift Simulation with ${totalFloors} floors and ${totalLifts} lifts`;
+
+  const building = document.getElementById("building");
+
+  // Create building
+  generateBuilding(totalFloors, totalLifts);
+}
+
+function generateBuilding(totalFloors, totalLifts) {
+  const building = document.getElementById("building");
+  building.innerHTML = "";
+
+  for (let floor = totalFloors; floor >= 0; floor--) {
+    const floorDiv = document.createElement("div");
+    floorDiv.className = "floor";
+
+    // Floor control system
+    const floorControl = `
+      <div class="floor-lift-control">
+        <div class="floor-lift-control-screen">
+          <div class="floor-lift-control-screen-up">^</div>
+          <div class="floor-lift-control-screen-text">${floor}</div>
+          <div class="floor-lift-control-screen-down">v</div>
+        </div>
+        <div class="floor-lift-control-button">
+          <button class="floor-lift-control-button-up">Up</button>
+          <button class="floor-lift-control-button-down">Down</button>
+        </div>
+      </div>`;
+
+    floorDiv.innerHTML += floorControl;
+
+    // Floor lifts
+    const floorLiftsDiv = document.createElement("div");
+    floorLiftsDiv.className = "floor-lifts";
+
+    for (let lift = 0; lift < totalLifts; lift++) {
+      const floorLiftDiv = document.createElement("div");
+      floorLiftDiv.className = "floor-lift";
+      floorLiftsDiv.appendChild(floorLiftDiv);
+    }
+
+    floorDiv.appendChild(floorLiftsDiv);
+
+    // Floor number
+    const floorNumberDiv = document.createElement("div");
+    floorNumberDiv.className = "floor-number";
+    if (floor === 0) {
+      floorNumberDiv.innerHTML = `<div>Ground Floor</div>`;
+    } else if (floor === 1) {
+      floorNumberDiv.innerHTML = `<div>${floor}st Floor</div>`;
+    } else if (floor === 2) {
+      floorNumberDiv.innerHTML = `<div>${floor}nd Floor</div>`;
+    } else if (floor === 3) {
+      floorNumberDiv.innerHTML = `<div>${floor}rd Floor</div>`;
+    } else {
+      floorNumberDiv.innerHTML = `<div>${floor}th Floor</div>`;
+    }
+
+    floorDiv.appendChild(floorNumberDiv);
+
+    // Append floor to building
+    building.appendChild(floorDiv);
+  }
 }
